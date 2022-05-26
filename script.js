c0eb0e190de3ce01287ea7c28be9b92fd5cd6143
Elyorbek <temirovelyor7347@gmail.com>
2022-05-26 23:45:43 +0500

    the end

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 "use strict";
+const api={
+    key:"a55fddb3602bee5a2e0e4e1c0149a975",
+    baseurl:'https://api.openweathermap.org/data/2.5/',
+};
+const searchBox=document.querySelector('.search-box');
+searchBox.addEventListener('keypress',setQuery)
+
+function setQuery(e){
+    if(e.keyCode===13){
+        getResults(searchBox.value)
+        console.log(searchBox.value);
+    }
+}
+function getResults(query){
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather=>{
+        return weather.json()
+    }).then(displayResults)
+}
+function displayResults(weather){
+    console.log(weather);
+    let city=document.querySelector('.city');
+    city.innerHTML=`${weather.name}, ${weather.sys.country}`;
+    
+    let now = new Date();
+    let date=document.querySelector('.date');
+    date.innerHTML=dateBuilder(now);
+    
+    let temp=document.querySelector('.temp');
+    temp.innerHTML=`${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let weatherEl=document.querySelector('.weather');
+    weatherEl.innerHTML=weather.weather[0].main;
+
+    let hilow=document.querySelector('.hi-low');
+    hilow.innerHTML=`${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+
+    searchBox.value='';
+}
+function dateBuilder(t){
+    let months=['January','February','March','April','May','June','July','August','September','October','November','December'];
+    let days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let day=days[t.getDay()];
+    let date=t.getDate();
+    let month=months[t.getMonth()];
+    let year=t.getFullYear();
+    return `${day} ${date} ${month} ${year}`
+}
